6b83073e4f17ac019bdcc8835eef5b391dc2779d
Felipe Santos <flpsno@hotmail.com>
2018-11-10 22:17:57 -0200

    adicionado mod01

---
+++ Mod. 01/FN.Store/FN.Store.UI/Controllers/ProdutosController.cs
@@ -0,0 +1,26 @@
+﻿using FN.Store.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FN.Store.UI.Controllers
+{
+    public class ProdutosController: Controller
+    {
+
+        public IActionResult Index()
+        {
+            var produtos = new List<Produto>() {
+                new Produto(){ Id = 1, Nome = "Picanha", TipoProduto = "Alimento", Preco=90.5M },
+                new Produto(){ Id = 2, Nome = "Pasta de Dente Colgate", TipoProduto = "Higiene", Preco=9.5M },
+                new Produto(){ Id = 3, Nome = "Fraldas Pampers", TipoProduto = "Higiene", Preco=80.5M },
+            };
+
+            return View(produtos);
+        }
+         
+    }
+}
